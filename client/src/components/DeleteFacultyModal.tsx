d4bd2928c464f90b0758a047ecd6f26707a8a775
Mitka Panarin <114948515+mitkapanarin@users.noreply.github.com>
2023-05-30 18:04:31 +0200

    frontend work in progress

---
+++ client/src/components/DeleteFacultyModal.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DeleteFacultyModal = () => {
+  return (
+    <div>DeleteFacultyModal</div>
+  )
+}
+
+export default DeleteFacultyModal
---
+++ client/src/components/EditFacultyModal.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EditFacultyModal = () => {
+  return (
+    <div>EditFacultyModal</div>
+  )
+}
+
+export default EditFacultyModal
